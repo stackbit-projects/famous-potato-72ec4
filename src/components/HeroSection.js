20e0205d7aab40b4606263712630ea39c9f92aae
Stackbit Code Editor <projects@stackbit.com>
2021-09-19 12:50:51 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -62,7 +62,7 @@ export default class HeroSection extends React.Component {
                             >
                                 {videoEmbed ? htmlToReact(videoEmbed)
                                     : <img
-                                            src={withPrefix()}
+                                            src={withPrefix(computer)}
                                             alt={imageAlt}
                                             className={classNames({
                                                 'mx-auto': alignX === 'center',
